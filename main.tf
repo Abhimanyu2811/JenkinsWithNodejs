34bd2c6ef2b4fb2e65e9f3803cab4d25d1a4ba0b
Aryan-Raj-Singh-Rathore <aryanrajsinghrathore.it25@gmail.com>
2025-04-25 23:11:13 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -14,36 +14,118 @@ provider "azurerm" {
   features {}
 }
 
-
+# Variables
 variable "location" {
   default = "East US"
 }
 
 variable "resource_group_name" {
-  default = "rg-storage-demo"
+  default = "myResourceGroup"
+}
+
+variable "acr_name" {
+  default = "acrrathore01" 
+}
+
+variable "aks_cluster_name" {
+  default = "myAKSCluster"
+}
+
+variable "dns_prefix" {
+  default = "myakscluster"
+}
+
+variable "node_count" {
+  default = 2
 }
 
 variable "storage_account_name" {
-  default = "rathorestorageacct1"
+  default = "rathorestorage01"
 }
 
-variable "container_name" {
-  default = "myblobcontainer"
-}
-
-resource "azurerm_resource_group" "example" {
+# Resource Group
+resource "azurerm_resource_group" "rg" {
   name     = var.resource_group_name
   location = var.location
 }
 
-resource "azurerm_storage_account" "example" {
+# Azure Container Registry (ACR)
+resource "azurerm_container_registry" "acr" {
+  name                = var.acr_name
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Basic"
+  admin_enabled       = true
+}
+
+# Azure Kubernetes Service (AKS)
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = var.aks_cluster_name
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = var.dns_prefix
+
+  default_node_pool {
+    name       = "default"
+    node_count = var.node_count
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "azure"
+    load_balancer_sku = "standard"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+# Role assignment to allow AKS to pull from ACR
+resource "azurerm_role_assignment" "aks_acr_pull" {
+  principal_id         = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name = "AcrPull"
+  scope                = azurerm_container_registry.acr.id
+
+  depends_on = [
+    azurerm_kubernetes_cluster.aks
+  ]
+}
+
+# Azure Storage Account
+resource "azurerm_storage_account" "storage" {
   name                     = var.storage_account_name
-  resource_group_name      = azurerm_resource_group.example.name
-  location                 = azurerm_resource_group.example.location
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "LRS"
 
   tags = {
-    environment = "dev"
+    Environment = "Development"
   }
+}
+
+# Outputs
+output "resource_group_name" {
+  value = azurerm_resource_group.rg.name
+}
+
+output "acr_login_server" {
+  value = azurerm_container_registry.acr.login_server
+}
+
+output "aks_cluster_name" {
+  value = azurerm_kubernetes_cluster.aks.name
+}
+
+output "storage_account_name" {
+  value = azurerm_storage_account.storage.name
+}
+
+output "storage_account_primary_endpoint" {
+  value = azurerm_storage_account.storage.primary_web_endpoint
 }
